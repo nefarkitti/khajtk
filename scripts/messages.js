b235bc3b4e3dedbc280ade0be68ad977c29cda40
Fire <17692105+FireMario211@users.noreply.github.com>
2023-08-19 01:28:00 +0000

    new domain

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,7 +1,7 @@
 let sortedFriends = [];
 
 function gotoProfile() {
-    window.location.href = `https://nefarkitti.github.io/khajtk/g/${currentUser.username}`
+    window.location.href = `https://social.nyaco.tk/g/${currentUser.username}`
 }
 
 //chatgpt be like:
